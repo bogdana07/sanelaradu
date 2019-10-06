5510379a13867eb89c2dc335d13f34a629b5dead
Sanela Radu <30578505+bogdana07@users.noreply.github.com>
2019-10-06 16:58:28 -0500

    added numbers and uppercase options to pw gen

--- HW3/app.js
+++ HW3/app.js
@@ -1,15 +1,13 @@
 var pw = [];
-var pwLen = prompt('How long is your password?');
-var letters = ['a', 'b', 'c', 'd', 'e', 'f'];
-var numbers = ['1', '2', '3', '4', '5'];
-var symbols = ['!', '@', '#', '$'];
-// var pwLen = prompt('what is your pw length? Must be between 8 and 128 characters.');
-var isSymbols = true;
-var isNumbers = true;
-var genBtn = document.getElementById('genPass');
+var pwLen = prompt('What is your password length? Must be 8-128 characters.');
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+'];
+var isSymbols = confirm('Do you want to include symbols?');
+var isNumbers = confirm('Do you want to include numbers?');
+var uppercase = confirm('Do you want to include uppercase letters?');
 
-
-// randomize an array
+// defined function to randomize the array
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -29,52 +27,53 @@ function shuffle(array) {
     return array;
 };
 
-//define function
+// defined function to generate password
 function createPassword() {
-    var pw = [];
 
+    //conditional: length > maximum 128 characters
     if (pwLen > 128) {
-        pwLen = prompt('Your password is greater than maximum 128 characters. Choose another password length');
+        pwLen = prompt('Invalid: Exceeds maximum 128 characters. Choose password length 8-128 characters');
         createPassword();
-    };
+    }
+    //conditional: length < minimum 8 characters
+    else if (pwLen < 8) {
+        pwLen = prompt('Invalid: Must be at least 8 characters. Choose password length 8-128 characters');
+        createPassword();
+    }
 
     for (i = 0; i < pwLen; i++) {
 
-        // if/else symbols
+        //if symbols
         if (isSymbols == true && i == 0) {
             var randomSymNo = Math.floor(Math.random() * symbols.length);
-
-            // inject a symbol in the string
-            // option 1
-            // pw = pw + symbols[randomSymNo];
-
-            // option 2
+            //inject symbol in string
             pw.push(symbols[randomSymNo]);
         }
 
-        // else/if numbers 
-        else if (isNumbers == true && i == 0) {
+        // if numbers 
+        if (isNumbers == true && i == 0) {
             var randomNum = Math.floor(Math.random() * numbers.length);
             pw.push(numbers[randomNum]);
         }
 
-        // else letters
-        else {
+        //if uppercase letters
+        if (uppercase == true && i == 0) {
             var randomNo = Math.floor(Math.random() * letters.length);
-            // option 1
-            pw.push(letters[randomNo]);
-
-            // option 2
-            // pw = pw + letters[randomNo];
+            pw.push(letters[randomNo].toUpperCase());
         }
 
-        pwShuffle = shuffle(pw);
-        pwBox.innerHTML = pwShuffle.toString();
+        //else lowercase letters
+        else {
+            var randomNo = Math.floor(Math.random() * letters.length);
+            pw.push(letters[randomNo]);
+        }
     }
-};
 
-//first event
-genBtn.addEventListener('click', showAlert);
-//change this password into a dynamic random pw
-//document.write(pw);
+    //randomize array pw
+    pwShuffle = shuffle(pw);
+    //write pw to html div
+    pwBox.innerHTML = pwShuffle.toString();
+    //remove commas between array items
+    pwBox.innerHTML = pw.join('');
+};
 
