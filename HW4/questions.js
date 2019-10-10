23f69376cc52af944cac3d2b8336d4bc57dd6675
Sanela Radu <30578505+bogdana07@users.noreply.github.com>
2019-10-10 18:30:57 -0500

    start hw4

--- HW4/questions.js
+++ HW4/questions.js
@@ -1,15 +1,37 @@
-//length of array questions determines length of play-15s/q
-
 var questions = [
     {
-        title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
+       title: "Commonly used data types DO NOT include:",
+       choices: ["strings", "booleans", "alerts", "numbers"],
+       answer: "alerts"
     },
     {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+       title: "The condition in an if / else statement is enclosed within ____.",
+       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+       answer: "parentheses"
     },
-    ///etc.
-];
+    {
+       title: "In the HTML document, what tag establishes the link to your JavaScript file?",
+       choices: ["<js>", "<script>", "<javascript>", "<app>"],
+       answer: "<script>"
+    },
+    {
+       title: "How do you create a function?",
+       choices: ["function: myFunction()", "function = myFunction()", "myFunction(function)", "function myFunction()"],
+       answer: "function myFunction()"
+    },
+    {
+       title: "Which of these choices correctly starts a FOR loop?",
+       choices: ["for (i=0, i<10, i++)", "for (i=0 && i<10; i++)", "for (i=0; i<10; i++)", "for (i=10, i<10, i++)"],
+       answer: "for (i=0; i<10; i++)"
+    },
+    {
+       title: "Which event occurs when the user clicks on an HTML element?",
+       choices: ["onmouseclick", "onleftmouse", "onclick", "onmouse"],
+       answer: "onclick"
+    },
+    {
+       title: "How do you assign a variable a numerical value of 7?",
+       choices: ["var = 7;", "var myVar = 7;", "var myVar: 7;", "myVar = 7;"],
+       answer: "var myVar = 7;"
+    },
+  ];
